53003dc7f069ab58fc9c1d8dc830afbc9be4a29e
nikhilsampangi <nikhil.sampangi@gmail.com>
2019-11-16 17:00:55 +0530

    userprofile and wallet pages

--- src/User/Usr_wallet.js
+++ src/User/Usr_wallet.js
@@ -6,6 +6,25 @@ export default class Usr_wallet extends Component {
     return (
       <div>
         <Navbar />
+        <br /><br /><br /><br /><br /><br /><br />
+        <div className="container">
+          <div className="card">
+            <div className="card-header">
+              <div className="row">
+                <div className="col-6">
+                  <h3>Add Money to Wallet</h3>
+                </div>
+                <div className="col">
+                  <i className="fa fa-money-bill-wave" />
+                  &nbsp;&nbsp;&nbsp;
+                  <i className="fa fa-angle-double-right" />
+                  &nbsp;&nbsp;&nbsp;
+                  <i className="fa fa-coins" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
