8f742147b304133c29b17f1f4da166d0b4fca95f
PrashantRaj18198 <prashantraj18198@gmail.com>
2019-11-16 22:02:00 +0530

    Admin login merged with frontend

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -46,7 +46,7 @@ function register(req, res) {
                         Manager.create(managerData)
                         .then(manager => {
                             const gen_token = randomToken(55);
-                            email.send_verification_token(gen_token, manager.email);
+                            // email.send_verification_token(gen_token, manager.email);
                             var newValues = { $set: {token: gen_token} };
                             Manager.updateOne({
                                 _id: manager._id
@@ -63,13 +63,63 @@ function register(req, res) {
                                 console.log('error:' + err.message)
                             });
 
-                        res.json({status: "registed and a verification link is sent to your email"});
+                        res.json({status: "registed"});
                         })
-                        .catch()
+                        .catch(err => {
+                            var arr= Object.keys(err['errors'])
+                            var errors= []
+                            for(i in arr){
+                              errors.push(err['errors'][arr[i]].message);
+                            }
+                            console.log(errors)
+                            res.json({error: errors});
+                          })
+                    console.log(manager)
                     })
                 }
+                else {
+                    console.log('manager exist')
+                }
+            })
+            .catch(err => {
+                console.log({error: err})
             })
         }
     })
 }
 
+router.post('/login', login)
+
+function login(req, res) {
+    Manager.findOne({
+        email: req.body.email
+    })
+    .then(manager => {
+        if(manager) {
+            if (bcrypt.compareSync(req.body.hashedPassword, manager.hashedPassword)) {
+                // Passwords match
+                const payload = {
+                  _id: manager._id,
+                  email: manager.email
+                }
+                let token = jwt.sign(payload, process.SECRET_KEY, {
+                  algorithm: 'HS256',
+                  expiresIn: 1440
+                })
+                res.send(token)
+              } else {
+                // Passwords don't match
+                res.json({ error: 'Incorrect Password' })
+              }
+            } else {
+              res.json({ error: 'manager does not exist' })
+            }
+          })
+          .catch(err => {
+            res.send('error: ' + err)
+    });
+    
+}
+    
+// router.post('/addpumps/')
+module.exports = router;
---
+++ src/authentication/adminFunctions.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export const register = newUser => {
+    return axios.post('admin/register', {
+        username: newUser.username,
+        hashedPassword: newUser.hashedPassword,
+        email: newUser.email
+    })
+        .then(res => {
+            if (res.data.error) {
+                const check = {
+                    status: false,
+                    error: res.data.error
+                }
+                return check
+            }
+            else {
+                const check = {
+                    status: true,
+                    data: res.data
+                }
+                return check
+            }
+        })
+}
+
+export const login = user => {
+    return axios.post('admin/login', {
+        email: user.email,
+        hashedPassword: user.hashedPassword
+    })
+        .then(res => {
+            if (res.data.error) {
+                const check = {
+                    status: false,
+                    error: res.data.error
+                }
+                return check
+            }
+            else {
+                Cookies.set('usertoken', res.data)
+                const check = {
+                    status: true,
+                    data: res.data
+                }
+                return check
+            }
+        })
+}
